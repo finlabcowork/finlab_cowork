46585330621184a73e13035d2f900d94c70cb48f
Hung-Ching-Lee <f128930847@gmail.com>
2024-01-07 21:53:39 +0800

    modiffied co_tools

--- Utils/co_tools.py
+++ Utils/co_tools.py
@@ -2,6 +2,9 @@ from finlab import data
 import finlab
 import pickle
 import os
+import plotly.express as px
+from finlab.backtest import sim
+
 
 path = "D:/trade_data/finlab_db_m/"
 
@@ -29,3 +32,18 @@ def co_get(dataset):
         print("地端資料庫未發現資料:"+"\""+dataset+"\""+",改為從finlab下載...")
         load_cloud_save(path,dataset)
         load_prem(path,dataset)
+        
+        
+def co_event_analysis(buy,vol):
+    sell = (vol%23 ==0)
+    position = buy.hold_until(sell)
+    report = sim(position , resample="D", upload=False, position_limit=1/3, fee_ratio=0,tax_ratio=0, trade_at_price='open')
+    report_trades = report.get_trades()
+    #去除端值(持有區間最高30天)
+    report_trades_clean = report_trades.drop(report_trades.loc[report_trades["period"]>30].index)
+    # 計算平均
+    report_trades_groupby = report_trades_clean.groupby("period").mean().reset_index()
+
+    ##繪圖
+    fig = px.scatter(report_trades_groupby, x="period", y="return",color='return',trendline="lowess")
+    fig.show()
