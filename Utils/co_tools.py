bcb7fffeba7732cec7296049e4578fe84eb92baf
Hung-Ching-Lee <f128930847@gmail.com>
2024-01-09 03:09:39 +0800

    add co_tools

--- Utils/co_tools.py
+++ Utils/co_tools.py
@@ -4,7 +4,11 @@ import pickle
 import os
 import plotly.express as px
 from finlab.backtest import sim
-
+from finlab.tools.event_study import create_factor_data
+import tqdm
+import numpy as np 
+import pandas as pd
+from finlab.dataframe import FinlabDataFrame
 
 path = "D:/trade_data/finlab_db_m/"
 
@@ -15,11 +19,19 @@ def load_cloud_save(path:str, dataset:str) :
     manual_path =path+data_set_name_re+".pickle"
     dataset_df.to_pickle(manual_path)
         
+        
+        
+        
+        
 def load_prem(path:str ,dataset:str) :
     data_set_name_re = dataset.replace(":","_")
     manual_path =path+data_set_name_re+".pickle"
     dataset_df  = pickle.load(open(manual_path, 'rb'))
     return dataset_df
+
+
+
+
 
 def co_get(dataset):
     if not os.path.isdir(path):
@@ -34,13 +46,20 @@ def co_get(dataset):
         load_prem(path,dataset)
         
         
-def co_event_analysis_simple(buy,vol):
+        
+             
+        
+def co_event_analysis_real_trade(buy,p=0.07):
     """
-    正確做法應該是要追蹤符合條件的股票未來每日的報酬率變化,但較為麻煩且耗費效能
-    改為用買入之後,使用finlab的回測套件隨機出場,並繪出持股持時間與報酬分布圖,僅限於樣本數較多之事件
+    利用回測軟體+機率隨機賣出,模擬事件發生後的報酬率
+    機率隨機賣出:每一天隨機賣掉的機率為p = 0.07
     """
-    sell = (vol%23 ==0)
-    position = buy.hold_until(sell)
+    buy = FinlabDataFrame(buy)
+    random_sell = pd.DataFrame(np.random.choice(a=[False, True], p=[1-p, p],size=(buy.shape[0], buy.shape[1]))  , columns=buy.columns,index=buy.index)
+    random_sell
+
+    # 部位
+    position = buy.hold_until(FinlabDataFrame(random_sell))
     report = sim(position , resample="D", upload=False, position_limit=1/3, fee_ratio=0,tax_ratio=0, trade_at_price='open')
     report_trades = report.get_trades()
     #去除端值(持有區間最高30天)
@@ -52,4 +71,105 @@ def co_event_analysis_simple(buy,vol):
     fig = px.scatter(report_trades_groupby, x="period", y="return",color='return',trendline="lowess")
     fig.show()
     
-def co_event_analysis(buy,vol):
+    
+    
+    
+    
+def co_event_analysis(buy:"dataframe"):
+    """
+    用finlab原始程式碼改的,用以分析事件發生前後之報酬率變化
+    注意:
+    
+    1.記憶體量不夠可能會錯誤
+    2.cross over
+    
+    參考:
+    https://www.finlab.tw/event-study-usage/
+    https://doc.finlab.tw/reference/tools/
+    """
+    adj_close = co_get('etl:adj_close')
+    factor_data = create_factor_data(buy, adj_close, event=buy)
+    buy_time_distribution = pd.DataFrame(buy.sum(axis=1)).reset_index() 
+    buy_time_distribution.rename(columns = {0:'number of times'}, inplace = True)
+    buy_time_distribution
+    fig1 = px.area(buy_time_distribution, x="date", y="number of times",color="number of times",
+                 title="事件發生次數與日期分布")
+    fig1.show()
+    
+    #用加權指數當成benchmark,排除加權指數時間變因
+    benchmark = co_get('benchmark_return:發行量加權股價報酬指數')
+    benchmark_pct = benchmark.reindex(adj_close.index, method='ffill').pct_change()
+    stock_pct = adj_close.pct_change()
+    def get_period(df, date, sample):
+        i = df.index.get_loc(date)
+        return df.iloc[i+sample[0]: i+sample[1]].values
+    
+    #轉換成,獨立事件與時間報酬率
+    ret = []
+    sample_period=(-40, -20) #
+    estimation_period=(-15, 30)# 觀察事件前15日與後30日變化
+    for date, sid in tqdm.tqdm(factor_data.index):
+
+        X1, Y1 = get_period(benchmark_pct, date, sample_period)[:,0], \
+            get_period(stock_pct[sid], date, sample_period)
+        X2, Y2 = get_period(benchmark_pct, date, estimation_period)[:,0], \
+            get_period(stock_pct[sid], date, estimation_period)
+
+        # Run CAPM
+        cov_matrix = np.cov(Y1, X1)
+        beta = cov_matrix[0, 1] / cov_matrix[1, 1]
+        AR = np.array(Y2) - beta * X2
+        ret.append(AR)
+    #計算事件發生日前後的日報酬率變化
+    ret = pd.DataFrame(ret, columns=range(*estimation_period))
+    ret_df = pd.DataFrame(ret.mul(100).mean()).reset_index() 
+    ret_df_re = ret_df.rename(columns = {"index":"days",0:"return"})
+    ret_df_re
+    fig2 = px.bar(ret_df_re, x="days", y="return",color="return",
+                 title="事件發生日前後的日報酬率變化")
+    # fig.add_trace(go.Scatter(
+    #     x=list(ret_df_re["days"]),
+    #     y=list(ret_df_re["return"]),
+    #     xperiod="M1",
+    #     xperiodalignment="middle",
+    #     hovertemplate="%{y}%{_xother}"
+    # ))
+    fig2.show()
+    
+    #計算累計報酬率,並將事件發生日作基準點
+    accum_ret_df = pd.DataFrame(ret.mul(100).cumsum(axis=1).mean()).reset_index() 
+    accum_ret_df_re = accum_ret_df.rename(columns = {"index":"days",0:"return"})
+    accum_ret_df_re["return_accumulated"] = accum_ret_df_re["return"] -accum_ret_df_re.at[15,"return"]
+    std = ret.mul(100).cumsum(axis=1).std() * 0.1
+    accum_ret_df_re
+    fig3 = px.line(accum_ret_df_re, x="days", y="return_accumulated",
+             title="累計報酬率,以事件發生日作基準點")
+
+    fig3.show()
+    return accum_ret_df_re
+
+
+
+
+
+def often_use_list():
+    data_set_list = ["price:收盤價",\
+                 "dividend_tse:最近一次申報每股 (單位)淨值",\
+                 "price_earning_ratio:股價淨值比",\
+                 'fundamental_features:營業利益率',\
+                 "monthly_revenue:當月營收",\
+                 "monthly_revenue:上月營收",\
+                 "monthly_revenue:去年當月營收",\
+                 "financial_statement:每股盈餘",\
+                 "price:成交股數",\
+                 "financial_statement:股本",\
+                 "etl:market_value",\
+                 "financial_statement:投資活動之淨現金流入_流出",\
+                 "financial_statement:營業活動之淨現金流入_流出",\
+                 "fundamental_features:經常稅後淨利",\
+                 "financial_statement:股東權益總額",\
+                 "fundamental_features:營業利益成長率",\
+                 "financial_statement:研究發展費",\
+                 
+                ]
+    return data_set_list
